fc05270faf971378d4c382f2b3991ab0b6048c68
Klajd Belishaku <devklajd@proton.me>
2023-11-11 22:11:04 +0100

    more changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -322,7 +322,7 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'You can send me a direct e-mail for further communications as well',
   items: [
     {
       type: ContactType.Email,
